3e2a0385d595e501aa823f7af2fa5722f11c2c42
Rishabh Raj <rishabhraj240405@gmail.com>
2024-04-07 06:44:45 +0530

    Add files via upload

---
+++ main.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void square(int* const x);
+
+int main()
+{
+	int *num = (int *)malloc(sizeof(int));
+	*num = 24;
+
+	square(num);
+	printf("The square of the given number is %d\n", *num);
+
+	return 0;
+}
+
+void square(int* x)
+{
+	*x = (*x) * (*x);
+}
